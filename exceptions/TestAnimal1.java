8833e5e0b432e8e38c85908709c941454adc2545
Varun-B210 <89086127+Varun-B210@users.noreply.github.com>
2022-07-15 14:11:37 +0530

    Exception practice code

---
+++ exceptions/TestAnimal1.java
@@ -0,0 +1,21 @@
+package exceptions;
+
+public class TestAnimal1 {
+
+	public static void main(String[] args) {
+		Animal [] animals = null;
+		test(animals);
+		animals = new Animal[2];
+		test(animals);
+	}
+	
+	private static void test(Animal[] animals) {
+		if(animals!=null)
+		for(Animal obj: animals) {
+			if(obj!=null) {
+			obj.eat();
+			obj.sleep();
+		}
+	}
+}
+}
