dcae5f1eaef2fe21921a0dbbf2dd4c79cc145482
Varun-B210 <89086127+Varun-B210@users.noreply.github.com>
2022-07-15 16:48:28 +0530

    String practice code

---
+++ string/StringEx.java
@@ -0,0 +1,27 @@
+package string;
+
+import java.util.Arrays;
+
+public class StringEx {
+	public static void main(String[] args) {
+		
+			String[] arr1  = {"A", "B", "C", "D"};
+			String[] arr2 = {"One",null,"Two"};
+			String[] arr3 = {"Hello"};
+			String[] arr4 = {};
+			String[] arr5 = null;
+			
+			
+			System.out.println(arrayToString(arr1));
+			System.out.println(arrayToString(arr2));
+			System.out.println(arrayToString(arr3));
+			System.out.println(arrayToString(arr4));
+			System.out.println(arrayToString(arr5));
+}
+	
+	private static String arrayToString(String [] arr) {
+		
+		return Arrays.toString(arr);
+		
+	}
+}
